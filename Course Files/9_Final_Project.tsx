8c18c3347e967d18f6ba7b9e32d2554b1afdf6cb
Peter Davenport <pdavenport@redventures.com>
2019-12-20 12:25:56 -0500

    updated the final project and answers

--- Course Files/9_Final_Project.tsx
+++ Course Files/9_Final_Project.tsx
@@ -6,18 +6,16 @@
 // and here: https://medium.com/@Alserda/readable-ts-enum-actiontypes-in-react-redux-c105fa0a7c9b
 // and write a sentence or two explaining pros and cons of using enums in section 3.
 
-// 3.) Write a sentence on how to find the type definition for the JSX.Element we used in section 5
+// 3.) Write a sentence on how to find more information about the JSX.Element (used in section 5) in the type
+// definition file.
 
 // 4.) Take a look at Union types: http://www.typescriptlang.org/docs/handbook/advanced-types.html#union-types
 // and write a few sentences on the caveats of using unions to describe multiple interfaces (like we did in section 7)
 
-// 5.) Why is writing:
+// 5.) What is an alternative to writing 'typeof deleteTodo' in our interface here?
 interface AppProps {
   todos: Todo[];
   fetchTodos: Function;
   deleteTodo: typeof deleteTodo;
 }
-// the best way to define deleteTodo in section 8? Try to figure out another way to define deleteTodo
-// in your project without any errors!
-
-// Submit your final answers and questions to:
+// Bonus: what does typeof automatically do for us when we use it?
